d44d91ec58b7768cff4db3c8cce1f83bbc1ec672
Paige <larsonpel181@gmail.com>
2023-08-27 23:25:09 -0400

    delete resume page, not button!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
-import Resume from './components/Resume';
 
 function App(){
   return(
@@ -17,7 +16,6 @@ function App(){
         <Route path="about" element={<About/>}/>
         <Route path="projects" element={<Projects/>}/>
         <Route path="contact" element={<Contact/>}/>
-        <Route path="resume" element={<Resume/>}/>
       </Route>
     </Routes>
     </>
